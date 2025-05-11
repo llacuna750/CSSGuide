d3d43bc4c649b45ef81ca41ccdcde0c6f375ac86
llacuna750 <llacuna750@gmail.com>
2025-05-11 10:15:03 +0800

    add function js

--- Javascript/solojs.js
+++ Javascript/solojs.js
@@ -63,9 +63,52 @@ const readp1 = document.getElementById(`p1`).textContent = "Updated: I want to b
 console.log(readp1);
 
 let pharagraph2 = document.getElementById(`p2`);
-alert(pharagraph2.textContent);
+// alert(pharagraph2.textContent);
 pharagraph2 = "New Content";
 console.log(pharagraph2);
+
+let coder = `Gab junior dev`;
+coder = `Gab Senior dev`;
+
+console.log(coder);
+
+function doSomething() {
+  let x = "Game Over";
+  // alert(x);
+  console.log(x);
+}
+
+doSomething();
+
+function myFunction() {
+  let x = document.getElementById(`ph1`);
+  x.textContent = `Updated Text`;
+  x.style.border = `dashed`;
+  x.style.backgroundColor = `Green`;
+}
+
+const div_button = document.querySelector(".divcolor");
+div_button.style.border = `1px solid`;
+div_button.style.padding = `50px 0`;
+div_button.style.textAlign = `Center`;
+div_button.style.width = `50%`;
+div_button.style.marginTop = `5px`;
+
+let mybutton = document.getElementById(`button_style`);
+
+function changeColor() {
+  mybutton.style.color = `green`;
+}
+
+// Assign the click handler correctly
+// document.getElementById("button_style").onclick = changeColor;
+
+mybutton.onclick = function() {
+  changeColor();
+  console.log(mybutton);
+  div_button.style.backgroundColor = "yellow";
+};
+
 
 
 
